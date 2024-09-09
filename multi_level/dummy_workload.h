2b6cac7d0930bb54e9037de2d3dc63df7f5e2ec9
Minsung Kim <alstjd025@naver.com>
2024-09-10 02:34:05 +0900

    Add missing header.
    Changes to be committed:
    	new file:   multi_level/dummy_workload.h

---
+++ multi_level/dummy_workload.h
@@ -0,0 +1,56 @@
+#include <EGL/egl.h>
+#include <GLES3/gl31.h>
+
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+#include <fstream>
+#include <cstdlib>
+#include <signal.h>
+#include <random>
+#include <map>
+
+class Workload {
+ public:
+  Workload();
+  // Workload(int duration, int cpu, int gpu, bool random);
+  Workload(int cpu, int gpu, bool random);
+
+  ~Workload();
+
+  void GPU_Worker();
+  void CPU_Worker();
+
+  struct timespec start_time;
+  bool ignition = false;
+  bool cpu_ignition = false;
+  bool gpu_ignition = false;
+  bool terminate = false;
+  std::mutex mtx;
+  std::mutex cpu_mtx;
+  std::mutex gpu_mtx;
+
+  std::condition_variable cv;
+  std::condition_variable cpu_cv;
+  std::condition_variable gpu_cv;
+  
+  std::atomic_bool stop;
+  std::atomic_bool cpu_stop;
+  std::atomic_bool gpu_stop;
+  std::atomic_bool cpu_worker_termination;
+  std::atomic_bool gpu_worker_termination;
+
+
+ private:
+  // GPU workload pool
+  std::vector<std::thread> gpu_workload_pool;
+
+  // CPU workload pool
+  std::vector<std::thread> cpu_workload_pool;
+
+};  // class Workload
